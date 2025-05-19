f029afcfcf3cd84efcb7d0203c13f5b6485231ff
nissim shukrun <nisssim1996@gmail.com>
2025-05-19 11:31:53 +0300

    fix: ensure proper handling of login form submission and state updates

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,53 @@
+import { useAppDispatch, useAppSelector } from "../../store/store";
+import { fetchLoginUser } from "../../slices/authSlice";
+import { useState } from "react";
+
 const Login = () => {
-  return <div></div>;
+  const dispatch = useAppDispatch();
+  const { user, message } = useAppSelector((state) => state.auth);
+
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    dispatch(fetchLoginUser({ email: form.email, password: form.password }));
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            name="password"
+            type="password"
+            value={form.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+      {message && <p>{message}</p>}
+      {user && <p>{user.name}!</p>}
+    </div>
+  );
 };
 
 export default Login;
